e1e6449bbedbfd3d9210c8655822f5fc064883f8
Nelson Rodrigues <nelson@waveweb2.com>
2021-03-15 22:28:50 +0000

    Finish!

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let debug = false;
+
 const center = {
     x: canvas.width / 2,
     y: canvas.height / 2
 }
 
-const circlePos = {
+const ball = {
     x: center.x,
-    y: canvas.height,
+    y: canvas.height - 75,
+    speed: 0.33333,
+    radius: 75
 };
 
-const radius = 75;
 let baseLineY = canvas.height - 200;
-let topLineY = circlePos.y - radius;
-let minTopLineY = baseLineY - 10;
+let topLineY = ball.y - ball.radius;
 
 const basePointLeft = {
-    x: center.x - radius - 150,
+    x: center.x - ball.radius - 200,
     y: baseLineY
 };
 
 const basePointRight = {
-    x: center.x + radius + 150,
+    x: center.x + ball.radius + 200,
     y: baseLineY
 };
 
-let intersectionPoints =  {
+let intersectionPoints = {
     left: {
         x: basePointLeft.x,
         y: baseLineY,
@@ -49,7 +51,7 @@ function circle(x, y, r, fill) {
     } else {
         ctx.stroke();
     }
-    
+
     ctx.closePath();
 }
 
@@ -62,7 +64,7 @@ function line(x1, y1, x2, y2) {
 }
 
 function circleLineIntersection(x1, y1, x2, y2, cx, cy, r) {
-    const m = (y2 - y1) / (x2-x1);
+    const m = (y2 - y1) / (x2 - x1);
 
     const c = (-m * x1 + y1);
     const aprim = (1 + Math.pow(m, 2));
@@ -78,6 +80,7 @@ function circleLineIntersection(x1, y1, x2, y2, cx, cy, r) {
     const y2_intersection = m * x2_intersection + c;
 
     return {
+        intersects: (x1_intersection && y1_intersection && x2_intersection && y2_intersection),
         left: {
             x: x2_intersection,
             y: y2_intersection,
@@ -89,87 +92,79 @@ function circleLineIntersection(x1, y1, x2, y2, cx, cy, r) {
     };
 }
 
-function setup () {
+function setup() {
     center.y = canvas.height;
 }
 
 function update() {
-    circlePos.y -= 0.5;
+    ball.y -= ball.speed;
+    const drag = ball.speed * 0.5;
 
-    if ((circlePos.y + radius) < 0) {
-        circlePos.y = canvas.height;
+    if ((ball.y + ball.radius) < -ball.radius) {
+        ball.y = canvas.height - ball.radius;
+        topLineY = ball.y - ball.radius;
     }
 
-    topLineY = lerp(topLineY, circlePos.y - radius, 0.8);
-
-    if (topLineY <= minTopLineY) {
-        topLineY = minTopLineY;
+    if (topLineY <= baseLineY) {
+        topLineY -= drag;
+    } else {
+        topLineY -= ball.speed;
     }
 
-    const lineIntersection = circleLineIntersection(0, topLineY, canvas.width, topLineY, circlePos.x, circlePos.y, radius);
+    const lineIntersection = circleLineIntersection(0, topLineY, canvas.width, topLineY, ball.x, ball.y, ball.radius);
 
-    if (lineIntersection.left.x) {
+    if (lineIntersection.intersects) {
         intersectionPoints = lineIntersection;
     } else {
-        intersectionPoints.left.y += 1.5;
-        intersectionPoints.right.y += 1.5;
+        intersectionPoints.left.y += drag * 1.75;
+        intersectionPoints.right.y += drag * 1.75;
     }
 }
 
-function draw () {
+function draw() {
     requestAnimationFrame(draw);
 
     update();
 
     ctx.fillStyle = 'hotpink';
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillRect(0, basePointLeft.y, canvas.width, canvas.height - basePointLeft.y);
 
-    circle(circlePos.x, circlePos.y, radius, true);
-
-    ctx.fillStyle = 'black';
-    line(0, topLineY, canvas.width, topLineY);
-    circle(intersectionPoints.right.x, intersectionPoints.right.y, 4, true);
-    circle(intersectionPoints.left.x, intersectionPoints.left.y, 4, true);
+    circle(ball.x, ball.y, ball.radius, true);
+    ctx.fillRect(0, basePointLeft.y - 1, canvas.width, canvas.height - basePointLeft.y + 1);
 
     // Draw curve between points
-    ctx.fillStyle = 'green'
     drawCurve(intersectionPoints);
+
+    if (debug) {
+        ctx.fillStyle = 'black';
+        circle(intersectionPoints.right.x, intersectionPoints.right.y, 4, true);
+        circle(intersectionPoints.left.x, intersectionPoints.left.y, 4, true);
+        line(0, topLineY, canvas.width, topLineY);
+        ctx.fillStyle = 'hotpink'
+    }
 }
 
 function drawCurve(lineIntersect) {
-    const controlPointDeviance = 50;
-    
     ctx.beginPath();
-    
+
     // 1. Start on the intersection point on the left
     ctx.moveTo(lineIntersect.left.x, lineIntersect.left.y);
 
     // 2. Draw a curve to the base point on the left
-    ctx.quadraticCurveTo(lineIntersect.left.x - controlPointDeviance, basePointLeft.y, basePointLeft.x, basePointLeft.y);
+    ctx.quadraticCurveTo(lineIntersect.left.x, basePointLeft.y, basePointLeft.x, basePointLeft.y);
 
     // 3. Draw a line to the base point on the right
     ctx.lineTo(basePointRight.x, basePointRight.y);
 
     // 4. Draw a curve up to the intersection point on the right
-    ctx.quadraticCurveTo(lineIntersect.right.x + controlPointDeviance, basePointRight.y, lineIntersect.right.x, lineIntersect.right.y);
+    ctx.quadraticCurveTo(lineIntersect.right.x, basePointRight.y, lineIntersect.right.x, lineIntersect.right.y);
 
     // 5. Connect back to the intersection point on the left
-    ctx.moveTo(lineIntersect.right.x, lineIntersect.right.y);
-    ctx.quadraticCurveTo(
-        circlePos.x,
-        lineIntersect.left.y,
-        lineIntersect.left.x, 
-        lineIntersect.left.y
-    );
+    ctx.lineTo(lineIntersect.left.x, lineIntersect.left.y);
 
     // 6. Close and fill the final shape
     ctx.fill();
     ctx.closePath();
-}
-
-function lerp(start, end, t) {
-  return (1 - t) * start + end * t;
 }
 
 setup();
